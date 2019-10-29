d903a1b5c4d6b4c65c34483d2eb10b883d40f5cd
RaresMihalcea <rares.mihalcea.mail@gmail.com>
2019-10-29 14:05:38 +0000

    generated navbar pages, removed list page

---
+++ src/app/instructors/instructors.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { InstructorsPage } from './instructors.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InstructorsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [InstructorsPage]
+})
+export class InstructorsPageModule {}
---
+++ src/app/instructors/instructors.page.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InstructorsPage } from './instructors.page';
+
+describe('InstructorsPage', () => {
+  let component: InstructorsPage;
+  let fixture: ComponentFixture<InstructorsPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InstructorsPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InstructorsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
